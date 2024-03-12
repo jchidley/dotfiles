e0d6f55badb311fdb4ee9a3ba857f7dec44b313a
Jack Chidley <7399749+jchidley@users.noreply.github.com>
2024-03-12 11:58:55 +0000

    ripgrep config

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -2,6 +2,11 @@ if (Test-Path $env:USERPROFILE/.ssh/id_ed25519) {
 		ssh-add $env:USERPROFILE/.ssh/id_ed25519
 }
 
+# powershell scripts
+$env:path += ";c:\tools\posh"
+
+# ripgrep profile
+$env:RIPGREP_CONFIG_PATH="$env:USERPROFILE/.ripgreprc"
+
+# needs to be last, just in case you Ctrl-C to abort it
 Invoke-Expression (& { (zoxide init powershell | Out-String) })
-
-
